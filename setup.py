d15b165c1e68d8c04d19faf44cd387046a4988a2
Yue Wang <xfdywy@users.noreply.github.com>
2021-05-14 17:32:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
                       'h5py', 
                       'termcolor', # adept_envs dependency
                       'click',  # adept_envs dependency
-                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
+                      'dm_control',
                       'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
